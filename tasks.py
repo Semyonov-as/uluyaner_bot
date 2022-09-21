9a78afd8a598dc08e5d1eb8622599f58af646be0
Semenov Alexandr <semyonov.as@phystech.edu>
2022-09-22 00:20:41 +0300

    nothing to do here

--- tasks.py
+++ tasks.py
@@ -6,7 +6,13 @@ class BotTasks:
         self.u_id = u_id
 
     async def add_task(self, text, freq): # everyday 9:00 take pills
-        text = 
-        self.freq = 
+        text = text.split()
+        self.freq = ""
+        if text[0] == 'everyday':
+            self.freq += ';'
+        else:
+            self.freq +=f"{text[0]};"
+
+        self.freq += text[1]
         self.task = lambda _ : self.bot.send_message(self.u_id, text)
 
